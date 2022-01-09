7a53ed10ff1637af4927bd9cafa5c8026b4c809c
KAMCIO240 <57112488+KAMCIO240@users.noreply.github.com>
2022-01-09 16:37:24 +0100

    Update Save the World Daily.py

--- Save the World Daily.py
+++ Save the World Daily.py
@@ -668,4 +668,4 @@ async def slashdaily(ctx, token=''):
 
 # noinspection SpellCheckingInspection
 client.loop.create_task(update_status())
-client.run('OTI5NzU0ODA1MjEwMDU0NzI3.Ydr7ow.E8vmrSTqReFskljWLxRGDww4zS0')
+client.run('')
